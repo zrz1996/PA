2e6f8ef470b95ffa5d1a7bc77a9ade5cf1a6c866
tracer-ics2015 <tracer@njuics.org>
2015-09-16 13:15:21 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  13:15:21 up 12:36,  3 users,  load average: 0.00, 0.01, 0.05 e48c42f45acb22a8cf33f379b68424248088869

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -62,7 +62,7 @@ static int cmd_info(char *args) {
 	if (strcmp(args, "r") == 0)
 	{
 		for (int i = 0; i < 8; i++)
-			printf("%x     %d", reg_l(i), reg_l(i));
+			printf("%x     %d\n", reg_l(i), reg_l(i));
 	}
 	return 0;
 }
