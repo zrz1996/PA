787a3dfe119986e2365427e30fe7726361d4609b
tracer-ics2015 <tracer@njuics.org>
2015-11-24 09:07:58 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  09:07:58 up 11:49,  3 users,  load average: 0.00, 0.01, 0.05 fad0fc36261ee10c463dadab77298cf563244ee

--- nemu/src/cpu/decode/decode-template.h
+++ nemu/src/cpu/decode/decode-template.h
@@ -35,7 +35,6 @@ make_helper(concat(decode_si_, SUFFIX)) {
 	//panic("please implement me");
 	op_src->simm = instr_fetch(eip, DATA_BYTE);
 	op_src->val = op_src->simm;
-	printf("0x%0x\n", op_src->val);
 #ifdef DEBUG
 	snprintf(op_src->str, OP_STR_SIZE, "$0x%x", op_src->val);
 #endif
