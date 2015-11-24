634a7d67f7a7f448407b9d693e45683cf77aef21
tracer-ics2015 <tracer@njuics.org>
2015-11-24 08:29:38 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  08:29:38 up 11:20,  3 users,  load average: 0.08, 0.03, 0.05 e1f5ea9b295848a6fdda255d4d16179ab5b72a6b

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -12,6 +12,7 @@
 #include "arith/div.h"
 #include "arith/sub.h"
 #include "arith/add.h"
+#include "arith/cmp.h"
 
 #include "logic/and.h"
 #include "logic/or.h"
---
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -0,0 +1,18 @@
+#include "cpu/exec/template-start.h"
+#include "../alu.h"
+#define instr cmp
+static void do_execute () {
+	//DATA_TYPE result = op_dest->val & op_src->val;
+	cpu.cf = 0;
+	alu(op_dest->val, op_src->val, 1, 1);
+	print_asm_template2();
+}
+
+make_instr_helper(i2rm)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+#include "cpu/exec/template-end.h"
