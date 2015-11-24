cbe0b7555ac58fe97da3adfd84cecf48496072b7
tracer-ics2015 <tracer@njuics.org>
2015-11-24 15:02:38 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  15:02:38 up 15:50,  3 users,  load average: 0.00, 0.01, 0.05 b8de189b4ad99bbe83368372eaf814e4a0c124e4

--- nemu/src/cpu/exec/control/jbe-template.h
+++ nemu/src/cpu/exec/control/jbe-template.h
@@ -4,9 +4,13 @@
 static void do_execute()
 {
 	if (cpu.cf == 1 || cpu.zf == 1)
-		cpu.eip += op_src->val;
-	int size = DATA_BYTE;
-	printf("%d\n", size);
+	{
+		int tmp = op_src->val;
+		#if DATA_BYTE == 1
+			tmp = (int32_t)((int8_t)tmp);
+		#endif
+		cpu.eip += tmp;
+	}
 	print_asm_template1();
 }
 make_instr_helper(i)
