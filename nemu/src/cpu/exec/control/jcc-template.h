7296b0259c78365db3b7c4d4a35a8009fa03fae5
tracer-ics2015 <tracer@njuics.org>
2015-11-25 11:31:10 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  11:31:10 up 1 day,  8:43,  3 users,  load average: 0.00, 0.01, 0.05 7aa3cfea6b72494b2de9e1e9bf492b213f25a3f6

--- nemu/src/cpu/exec/control/jcc-template.h
+++ nemu/src/cpu/exec/control/jcc-template.h
@@ -19,4 +19,305 @@ static void do_execute()
 }
 make_instr_helper(i)
 #undef instr
+
+#define instr ja
+static void do_execute()
+{
+	if (cpu.zf == 0 || cpu.cf == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jae
+static void do_execute()
+{
+	if (cpu.cf == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jb
+static void do_execute()
+{
+	if (cpu.cf == 1)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jc
+static void do_execute()
+{
+	if (cpu.cf == 1)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jcxz
+static void do_execute()
+{
+	if (REG(R_CX) == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jecxz
+static void do_execute()
+{
+	if (REG(R_ECX))
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jz
+static void do_execute()
+{
+	if (cpu.zf == 1)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jg
+static void do_execute()
+{
+	if (cpu.zf == 0 && cpu.sf == cpu.of)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jge
+static void do_execute()
+{
+	if (cpu.sf == cpu.of)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jl
+static void do_execute()
+{
+	if (cpu.sf != cpu.of)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jle
+static void do_execute()
+{
+	if (cpu.zf == 1 || cpu.sf != cpu.of)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jna
+static void do_execute()
+{
+	if (cpu.zf == 1 || cpu.cf == 1)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jnae
+static void do_execute()
+{
+	if (cpu.cf == 1)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jnb
+static void do_execute()
+{
+	if (cpu.cf == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jnbe
+static void do_execute()
+{
+	if (cpu.zf == 0 || cpu.cf == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jnc
+static void do_execute()
+{
+	if (cpu.cf == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jne
+static void do_execute()
+{
+	if (cpu.zf == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jng
+static void do_execute()
+{
+	if (cpu.zf == 1 || cpu.sf != cpu.of)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jnge
+static void do_execute()
+{
+	if (cpu.zf == 1 || cpu.sf != cpu.of)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jnl
+static void do_execute()
+{
+	if (cpu.sf == cpu.of)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jnle
+static void do_execute()
+{
+	if (cpu.zf == 0 || cpu.sf == cpu.of)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jno
+static void do_execute()
+{
+	if (cpu.of == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jnp
+static void do_execute()
+{
+	if (cpu.pf == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jns
+static void do_execute()
+{
+	if (cpu.sf == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jnz
+static void do_execute()
+{
+	if (cpu.zf == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jo
+static void do_execute()
+{
+	if (cpu.of == 1)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jp
+static void do_execute()
+{
+	if (cpu.pf == 1)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jpe
+static void do_execute()
+{
+	if (cpu.pf == 1)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr jpo
+static void do_execute()
+{
+	if (cpu.pf == 0)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
+#define instr js
+static void do_execute()
+{
+	if (cpu.sf == 1)
+		cpu.eip += signExtend(op_src->val, DATA_BYTE);
+	print_asm_template1();
+}
+make_instr_helper(i)
+#undef instr
+
 #include "cpu/exec/template-end.h"
