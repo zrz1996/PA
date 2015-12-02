76f7e3f1600083511144af15bab16470c9038654
tracer-ics2015 <tracer@njuics.org>
2015-12-02 17:13:53 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  17:13:53 up 35 min,  4 users,  load average: 0.00, 0.01, 0.05 1a553aba508d737764582146493a75d2742c5631

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -103,7 +103,7 @@ helper_fun opcode_table [256] = {
 /* 0x1c */	inv, inv, inv, inv,
 /* 0x20 */	inv, inv, inv, inv,
 /* 0x24 */	inv, inv, inv, inv,
-/* 0x28 */	inv, inv, inv, inv,
+/* 0x28 */	inv, inv, inv, sub_rm2r_v,
 /* 0x2c */	inv, inv, inv, inv,
 /* 0x30 */	inv, inv, inv, inv,
 /* 0x34 */	inv, inv, inv, inv,
