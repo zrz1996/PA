466e821ef852b28b66ad835a403f0da06a7135b4
tracer-ics2015 <tracer@njuics.org>
2016-02-23 19:52:30 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  19:52:30 up  2:55,  3 users,  load average: 0.00, 0.02, 0.05 f507e453973204ebf660e68b9ca716aa8346a4

--- nemu/src/cpu/exec/control/push-template.h
+++ nemu/src/cpu/exec/control/push-template.h
@@ -7,13 +7,8 @@ static void do_execute()
 	if (DATA_BYTE == 2)
 		REG(R_ESP) -= 2;
 	else
-	{
 		REG(R_ESP) -= 4;
-		printf("%x\n", REG(R_ESP));
-	}
-	printf("%x %x\n", REG(R_ESP), op_src->val);
 	MEM_W(REG(R_ESP), op_src->val);
-	printf("%x\n", REG(R_ESP));
 	print_asm_template1();
 }
 make_instr_helper(r)
