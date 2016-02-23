028013b00c30f5971f7568b1cb2fd13cc8016076
tracer-ics2015 <tracer@njuics.org>
2016-02-23 13:20:19 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  13:20:19 up  1:28,  3 users,  load average: 0.08, 0.03, 0.05 687060dc174e5ba5ea606ee971a4bd9a95f27a66

--- nemu/src/cpu/exec/logic/and-template.h
+++ nemu/src/cpu/exec/logic/and-template.h
@@ -3,7 +3,7 @@
 #define instr and
 
 static void do_execute () {
-	DATA_TYPE result = op_dest->val & op_src->val;
+	DATA_TYPE result = op_dest->val & signExtend(op_src->val, DATA_BYTE);
 	OPERAND_W(op_dest, result);
 	cpu.cf = 0;
 	alu(result, 0, 1, 0);
