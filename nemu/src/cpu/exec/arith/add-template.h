b2bf824a23cd39e9ab95f99ae803aeb9b7130be4
tracer-ics2015 <tracer@njuics.org>
2016-02-24 16:21:35 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:21:35 up 11:32,  3 users,  load average: 0.00, 0.06, 0.14 12b979a617e122e6beddc0f5cc3c34d914c7f6a1

--- nemu/src/cpu/exec/arith/add-template.h
+++ nemu/src/cpu/exec/arith/add-template.h
@@ -15,4 +15,13 @@ make_instr_helper(si2rm)
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 
+make_helper(concat(add_i2a_, SUFFIX)) {
+	concat(decode_i_, SUFFIX)(eip + 1);
+	op_dest->type = OP_TYPE_REG;
+	op_dest->reg = R_EAX;
+	op_dest->val = REG(R_EAX);
+	snprintf(op_dest->str, OP_STR_SIZE, "%%%s", REG_NAME(R_EAX));
+	do_execute();
+	return 2;
+}
 #include "cpu/exec/template-end.h"
