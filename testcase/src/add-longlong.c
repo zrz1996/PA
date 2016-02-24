d9a15972ec78b32cf246bc5adbbf515ce78bd3d4
tracer-ics2015 <tracer@njuics.org>
2016-02-24 19:41:38 +0800

    >  run 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  19:41:38 up 14:36,  3 users,  load average: 0.20, 0.09, 0.07 a5fd2c2e83b8c82febb4cd05164ff0ec6c5cf4ae

--- testcase/src/add-longlong.c
+++ testcase/src/add-longlong.c
@@ -12,8 +12,8 @@ long long ans[] = {0LL, 0x1LL, 0x2LL, 0x7fffffffffffffffLL, 0x8000000000000000LL
 
 int main() {
 	int i, j, ans_idx = 0;
-	for(i = 0; i < 1; i ++) {
-		for(j = 0; j < 2; j ++) {
+	for(i = 0; i < NR_DATA; i ++) {
+		for(j = 0; j < NR_DATA; j ++) {
 			nemu_assert(add(test_data[i], test_data[j]) == ans[ans_idx ++]);
 		}
 	}
