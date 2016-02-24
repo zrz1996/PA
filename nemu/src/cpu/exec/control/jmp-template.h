6ff39f19fd37463b5c354fc30e8dd0440724a062
tracer-ics2015 <tracer@njuics.org>
2016-02-24 16:37:20 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:37:20 up 11:48,  3 users,  load average: 0.00, 0.01, 0.05 3ba6e2a66dceca46b8955057ae5ae89606aa4a4

--- nemu/src/cpu/exec/control/jmp-template.h
+++ nemu/src/cpu/exec/control/jmp-template.h
@@ -4,11 +4,20 @@
 
 static void do_execute()
 {
-	cpu.eip += op_src->val;
-	#if DATA_BYTE == 2
-		cpu.eip &= 0x0000ffff;
-	#endif
+	if (ops_decoded.opcode == 0xff)
+	{
+		cpu.eip = op_src->val;
+		if (DATA_BYTE == 2)
+			cpu.eip &= 0x0000ffff;
+	}
+	else
+	{
+		cpu.eip += op_src->val;
+		if (DATA_BYTE == 2)
+			cpu.eip &= 0x0000ffff;
+	}
 	print_asm_template1();
 }
 make_instr_helper(i)
+make_instr_helper(rm)
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/control/jmp.h
+++ nemu/src/cpu/exec/control/jmp.h
@@ -3,4 +3,6 @@
 
 make_helper(jmp_i_v);
 make_helper(jmp_i_b);
+make_helper(jmp_rm_v);
+make_helper(jmp_rm_b);
 #endif
