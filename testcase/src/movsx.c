a63874e2854f973dc3edb66493002156bcbee4cb
tracer-ics2015 <tracer@njuics.org>
2016-02-24 15:52:36 +0800

    >  run 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  15:52:36 up 11:03,  3 users,  load average: 0.00, 0.01, 0.05 1598e715cdbd232afa256d9d43ec62b530f4c447

--- testcase/src/movsx.c
+++ testcase/src/movsx.c
@@ -4,7 +4,6 @@ int A[10];
 int b;
 char C[10];
 int main() {
-	/*
 	A[0] = 0;
 	A[1] = 1;
 	A[2] = 2;
@@ -13,15 +12,12 @@ int main() {
 
 	b = A[3];
 	A[5] = b;
-	*/
 	C[0] = 'a';
 	nemu_assert(C[0] == 'a');
 	C[1] = C[0];
 	nemu_assert(C[1] == 'a');
-	set_bp();
 	A[0] = (int)C[0];
 	nemu_assert(A[0] == 'a');
-	/*
 	C[1] = 0x80;
 	A[0] = (int)C[1];
 	nemu_assert(A[1] == 1);
@@ -32,7 +28,6 @@ int main() {
 	nemu_assert(A[5] == 3);
 	nemu_assert(C[1] == 0xffffff80);
 	nemu_assert(A[0] == 0xffffff80);
-	*/
 	HIT_GOOD_TRAP;
 
 	return 0;
