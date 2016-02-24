ae8d88507ba80a9ff9f6e4e52ce65277484b4575
tracer-ics2015 <tracer@njuics.org>
2016-02-24 17:24:12 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  17:24:12 up 12:35,  3 users,  load average: 0.00, 0.01, 0.05 d9e828a7917a5bfe28419adabe82b6a97073763

--- nemu/src/cpu/exec/all-instr.h
+++ nemu/src/cpu/exec/all-instr.h
@@ -16,6 +16,7 @@
 #include "arith/div.h"
 #include "arith/sub.h"
 #include "arith/add.h"
+#include "arith/adc.h"
 #include "arith/cmp.h"
 
 #include "logic/and.h"
---
+++ nemu/src/cpu/exec/arith/adc-template.h
@@ -0,0 +1,26 @@
+#include "cpu/exec/template-start.h"
+#include "../alu.h"
+#define instr adc
+static void do_execute () {
+	DATA_TYPE result = alu(op_dest->val, op_src->val, 1, 0);
+	OPERAND_W(op_dest, result);
+	print_asm_template2();
+}
+
+make_instr_helper(i2rm)
+#if DATA_BYTE == 2 || DATA_BYTE == 4
+make_instr_helper(si2rm)
+#endif
+make_instr_helper(r2rm)
+make_instr_helper(rm2r)
+
+make_helper(concat(adc_i2a_, SUFFIX)) {
+	concat(decode_i_, SUFFIX)(eip + 1);
+	op_dest->type = OP_TYPE_REG;
+	op_dest->reg = R_EAX;
+	op_dest->val = REG(R_EAX);
+	snprintf(op_dest->str, OP_STR_SIZE, "%%%s", REG_NAME(R_EAX));
+	do_execute();
+	return DATA_BYTE + 1;
+}
+#include "cpu/exec/template-end.h"
