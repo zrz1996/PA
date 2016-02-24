8139375c5843bd812927d62f472515d97a227b55
tracer-ics2015 <tracer@njuics.org>
2016-02-24 15:34:30 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  15:34:30 up 10:45,  3 users,  load average: 0.00, 0.01, 0.05 eb808cc8d81cf8195c1a3c156a33dc8a89b527d1

--- nemu/src/cpu/exec/control/push-template.h
+++ nemu/src/cpu/exec/control/push-template.h
@@ -8,8 +8,7 @@ static void do_execute()
 		reg_l(R_ESP) -= 2;
 	else
 		reg_l(R_ESP) -= 4;
-	printf("%d\n", op_src->val);
-	MEM_W(reg_l(R_ESP), op_src->val);
+	swaddr_write(reg_l(R_ESP), DATA_BYTE == 2 ? 2 : 4, op_src->val);
 	print_asm_template1();
 }
 make_instr_helper(r)
