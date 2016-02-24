01a2259f8b6350b0f3ed78f556c621792416f0c6
tracer-ics2015 <tracer@njuics.org>
2016-02-24 14:44:18 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  14:44:18 up  9:55,  3 users,  load average: 0.00, 0.01, 0.05 446138254243cdea570d60233903fa5f05276de

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -14,4 +14,14 @@ make_instr_helper(si2rm)
 make_instr_helper(r2rm)
 make_instr_helper(rm2r)
 
+make_helper(concat(cmp_i2a_, SUFFIX)) {
+	concat(decode_r_, SUFFIX)(eip);
+	op_dest->type = OP_TYPE_REG;
+	op_dest->reg = R_EAX;
+	op_dest->val = REG(R_EAX);
+	snprintf(op_dest->str, OP_STR_SIZE, "%s", REG_NAME(R_EAX));
+	do_execute();
+	return 1;
+}
+
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/arith/cmp.c
+++ nemu/src/cpu/exec/arith/cmp.c
@@ -13,7 +13,7 @@
 #undef DATA_BYTE
 
 /* for instruction encoding overloading */
-
+make_helper_v(cmp_i2a)
 make_helper_v(cmp_i2rm)
 make_helper_v(cmp_si2rm)
 make_helper_v(cmp_r2rm)
