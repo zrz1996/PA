2dafd540b944092f1b56c07501fc53759f06c018
tracer-ics2015 <tracer@njuics.org>
2016-02-25 12:09:49 +0800

    >  run 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  12:09:49 up 19:48,  3 users,  load average: 0.00, 0.01, 0.05 32491deb94a633a27fa2a90c6a50563bd3e89a17

--- testcase/src/struct.c
+++ testcase/src/struct.c
@@ -20,7 +20,7 @@ int main() {
 
 	//struct dummy t = fun(d);
 	for(i = 0; i < N; i ++) {
-		nemu_assert(d.pad1[i] == 0 + 128);
+		nemu_assert(d.pad1[i] == i + 128);
 		//nemu_assert(d.pad2[i] == i);
 	}
 	HIT_GOOD_TRAP;
