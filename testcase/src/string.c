0cd8f662adc3761ae19b46111a132ac948ef2558
tracer-ics2015 <tracer@njuics.org>
2016-02-25 15:08:28 +0800

    >  run 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  15:08:28 up 22:47,  3 users,  load average: 0.00, 0.01, 0.05 25f37d906b877f4aa871ffffba0ee97ef2b3a409

--- testcase/src/string.c
+++ testcase/src/string.c
@@ -5,8 +5,7 @@ char *s[] = {
 	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 
 	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaab",
 	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-	//", World!\n",
-	"a",
+	", World!\n",
 	"Hello, World!\n",
 	"#####"
 };
@@ -15,16 +14,13 @@ char str1[] = "Hello";
 char str[20];
 
 int main() {
-	/*
 	nemu_assert(strcmp(s[0], s[2]) == 0);
 	nemu_assert(strcmp(s[0], s[1]) == -1);
 	nemu_assert(strcmp(s[0] + 1, s[1] + 1) == -1);
 	nemu_assert(strcmp(s[0] + 2, s[1] + 2) == -1);
 	nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1);
 	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
-	*/
-	//nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
-	nemu_assert(strcat(str, "a")[0] == 'a');
+	nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 	HIT_GOOD_TRAP;
 	return 0;
 }
