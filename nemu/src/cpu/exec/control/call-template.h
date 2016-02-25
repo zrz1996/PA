e9966c25b20a1afe869b1572d4ac2bf0dc1315b3
tracer-ics2015 <tracer@njuics.org>
2016-02-25 16:40:03 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:40:03 up 1 day, 18 min,  3 users,  load average: 0.00, 0.01, 0.05 af6913961990f94b4579f4b0542c47d023a300e0

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -15,7 +15,7 @@ static void do_execute()
 make_instr_helper(i)
 
 make_helper(concat(call_rm_, SUFFIX)) {
-	concat(decode_rm_, SUFFIX)(cpu.eip + 1);
+	concat(decode_rm_, SUFFIX)(eip + 1);
 	REG(R_ESP) -= DATA_BYTE;
 	MEM_W(REG(R_ESP), cpu.eip + DATA_BYTE + 1);
 	cpu.eip = op_src->val;
