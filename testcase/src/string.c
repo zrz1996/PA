01ad42263bd1dcdd0c8e590ec29d004c9b3104ea
tracer-ics2015 <tracer@njuics.org>
2016-02-26 21:17:47 +0800

    >  run 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  21:17:47 up  4:40,  3 users,  load average: 0.05, 0.22, 0.23 43ed59c64c03524e2388209e9baeaf4dd0554d1e

--- testcase/src/string.c
+++ testcase/src/string.c
@@ -19,8 +19,8 @@ int main() {
 	nemu_assert(strcmp(s[0] + 1, s[1] + 1) == -1);
 	nemu_assert(strcmp(s[0] + 2, s[1] + 2) == -1);
 	nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1);
-	HIT_GOOD_TRAP;
 	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
+	HIT_GOOD_TRAP;
 	nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 	return 0;
 }
