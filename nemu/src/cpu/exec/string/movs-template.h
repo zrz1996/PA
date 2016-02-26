6c5b93fbc1ed94af9784d49f9d92616c8cd1a33a
tracer-ics2015 <tracer@njuics.org>
2016-02-26 16:49:49 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:49:49 up 12 min,  3 users,  load average: 0.16, 0.05, 0.06 12ccea5238e1638d577000a57794a99e7a3a5e

--- nemu/src/cpu/exec/string/movs-template.h
+++ nemu/src/cpu/exec/string/movs-template.h
@@ -3,18 +3,18 @@
 #define instr movs
 
 make_helper(concat(movs_, SUFFIX)) {
-	DATA_TYPE temp = swaddr_read(REG(R_ESI), DATA_BYTE);
+	DATA_TYPE temp = swaddr_read(reg_l(R_ESI), DATA_BYTE);
 	printf("%x\n", temp);
-	swaddr_write(REG(R_EDI), DATA_BYTE, temp);
+	swaddr_write(reg_l(R_EDI), DATA_BYTE, temp);
 	if (cpu.df == 0)
 	{
-		REG(R_ESI) += DATA_BYTE;
-		REG(R_EDI) += DATA_BYTE;
+		reg_l(R_ESI) += DATA_BYTE;
+		reg_l(R_EDI) += DATA_BYTE;
 	}
 	else
 	{
-		REG(R_ESI) -= DATA_BYTE;
-		REG(R_EDI) -= DATA_BYTE;
+		reg_l(R_ESI) -= DATA_BYTE;
+		reg_l(R_EDI) -= DATA_BYTE;
 	}
 	print_asm("movs");
 	return 1;
