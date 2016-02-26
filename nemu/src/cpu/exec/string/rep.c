227f3d616066f5d4f28de23bc30c5a7825a0f36e
tracer-ics2015 <tracer@njuics.org>
2016-02-26 17:21:01 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  17:21:01 up 43 min,  3 users,  load average: 0.00, 0.01, 0.05 a21e4002d1adb22b2fb68d4dd94cdd9c5b2e9d09

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -28,7 +28,6 @@ make_helper(rep) {
 			if (ops_decoded.opcode == 0xa6 || ops_decoded.opcode == 0xa7
 				|| ops_decoded.opcode == 0xae || ops_decoded.opcode == 0xaf)
 			{
-				printf("%x %d\n", ops_decoded.opcode, cpu.zf);
 				if ((rep_code == 0xf3 && cpu.zf == 0) || (rep_code == 0xf2 && cpu.zf == 1))
 					break;
 			}
