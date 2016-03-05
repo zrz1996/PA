5467603d46978bda1b694968e2f52a768b6df340
tracer-ics2015 <tracer@njuics.org>
2016-03-05 10:40:24 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  10:40:24 up 18 min,  2 users,  load average: 0.16, 0.05, 0.08 58b49fec33bf6dccae08255a6e35eec4b8fd02b9

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -4,8 +4,8 @@
 
 static void do_execute()
 {
-	REG(R_ESP) -= DATA_BYTE;
-	MEM_W(REG(R_ESP), cpu.eip + DATA_BYTE + 1);
+	reg_l(R_ESP) -= DATA_BYTE;
+	MEM_W(reg_l(R_ESP), cpu.eip + DATA_BYTE + 1);
 	cpu.eip += op_src->val;
 	#if DATA_BYTE == 2
 		cpu.eip &= 0x0000ffff;
@@ -15,15 +15,15 @@ static void do_execute()
 make_instr_helper(i)
 
 make_helper(concat(call_rm_, SUFFIX)) {
-	concat(decode_rm_, SUFFIX)(eip + 1);
-	REG(R_ESP) -= DATA_BYTE;
-	MEM_W(REG(R_ESP), cpu.eip + DATA_BYTE + 1);
-	cpu.eip = op_src->val;
+	int len = concat(decode_rm_, SUFFIX)(eip + 1);
+	reg_l(R_ESP) -= DATA_BYTE;
+	MEM_W(reg_l(R_ESP), cpu.eip + DATA_BYTE + 1);
+	cpu.eip = op_src->val - len - 1;
 	#if DATA_BYTE == 2
 		cpu.eip &= 0x0000ffff;
 	#endif
 	print_asm_template1();
-	return 0;
+	return len + 1;
 }
 
 #include "cpu/exec/template-end.h"
