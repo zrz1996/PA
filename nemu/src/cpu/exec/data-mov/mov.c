48f50f1204a2b9e59d9fe8d56d9317684dbe01f0
tracer-ics2015 <tracer@njuics.org>
2016-03-11 09:55:52 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  09:55:52 up 14:52,  3 users,  load average: 0.00, 0.01, 0.05 c65f2917ee9e8619614ab0a0a3cbb5b57a56a657

--- nemu/src/cpu/exec/data-mov/mov.c
+++ nemu/src/cpu/exec/data-mov/mov.c
@@ -23,18 +23,16 @@ make_helper_v(mov_moffs2a)
 
 make_helper(mov_c2r)
 {
-	uint32_t len = decode_rm_l(eip);
+	uint32_t len = decode_rm_l(eip + 1);
 	reg_l(op_src->reg) = cpu.cr0;
 	print_asm("mov" " CR0,%s", op_src->str);
-	return len;
+	return len + 1;
 }
 
 make_helper(mov_r2c)
 {
-	eip++;
-	uint32_t len = decode_rm_l(eip);
+	uint32_t len = decode_rm_l(eip + 1);
 	cpu.cr0 = reg_l(op_src->reg);
 	print_asm("mov" " %s,CR0", op_src->str);
-	printf("%x\n", len);
-	return len;
+	return len + 1;
 }
