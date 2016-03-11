524a95051d493ec66e0cad45e674e91499305320
tracer-ics2015 <tracer@njuics.org>
2016-03-11 13:04:54 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  13:04:54 up 18:01,  3 users,  load average: 0.00, 0.01, 0.05 f532c74a837f9a7488dea39ebf8ccc57ea8c2b

--- nemu/src/cpu/exec/control/jmp.c
+++ nemu/src/cpu/exec/control/jmp.c
@@ -20,22 +20,21 @@ make_helper_v(jmp_i)
 make_helper_v(jmp_rm)
 
 make_helper(ljmp) {
-	uint16_t cs_t = instr_fetch(eip + 1, 2);
-	uint32_t eip_t = instr_fetch(eip + 3, 4) - 7;
+	uint32_t eip_t = instr_fetch(eip + 1, 4) - 7;
+	uint16_t cs_t = instr_fetch(eip + 5, 2);
 	cpu.cs = cs_t;
 	cpu.eip = eip_t;
 	uint32_t gdt_addr = cpu.gdtr >> 16;
 	uint32_t index = cpu.cs >> 3;
 	index <<= 3;
 	gdt_addr += index;
-	uint64_t gdt = ((uint64_t)lnaddr_read(gdt_addr + 4, 4) << 32) | lnaddr_read(gdt_addr, 4);
 	union {
 		uint64_t gdt;
 		SegDesc SD;
 	}temp;
-	temp.gdt = gdt;
-	cpu.segbase[1] = (temp.SD.base_31_24 << 24) + (temp.SD.base_23_16 << 16) + (temp.SD.base_15_0);
+	temp.gdt = ((uint64_t)lnaddr_read(gdt_addr + 4, 4) << 32) | lnaddr_read(gdt_addr, 4);
 	printf("%x\n", cpu.segbase[1]);
+	cpu.segbase[1] = (temp.SD.base_31_24 << 24) + (temp.SD.base_23_16 << 16) + (temp.SD.base_15_0);
 	cpu.seglimit[1] = (temp.SD.limit_19_16 << 16) + temp.SD.limit_15_0;
 	print_asm("ljmp" " 0x%x,0x%x", cpu.cs, cpu.eip + 7);
 	return 7;
