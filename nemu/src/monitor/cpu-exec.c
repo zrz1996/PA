87b6ca884647af49ecda48875c24e4ec3913181f
tracer-ics2015 <tracer@njuics.org>
2016-03-11 12:38:47 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  12:38:47 up 17:35,  3 users,  load average: 0.18, 0.08, 0.06 a24b6bc08bca3ea968e40cd0dd9f58d233348cd3

--- nemu/src/monitor/cpu-exec.c
+++ nemu/src/monitor/cpu-exec.c
@@ -65,7 +65,7 @@ void cpu_exec(volatile uint32_t n) {
 		cpu.eip += instr_len;
 #ifdef DEBUG
 		print_bin_instr(eip_temp, instr_len, cr0_temp);
-		printf("####");
+		printf("####%x\n", cpu.eip);
 		strcat(asm_buf, assembly);
 		Log_write("%s\n", asm_buf);
 		if(n_temp < MAX_INSTR_TO_PRINT) {
