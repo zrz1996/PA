c9f4e08794ec97852e17519f53d6c0e4abb620e9
tracer-ics2015 <tracer@njuics.org>
2016-03-11 09:15:33 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  09:15:33 up 14:11,  3 users,  load average: 0.00, 0.01, 0.05 1130aaa1e2d6be9855b575ce34ff8536aecad1f5

--- nemu/src/cpu/exec/sys/lgdt.c
+++ nemu/src/cpu/exec/sys/lgdt.c
@@ -3,9 +3,9 @@
 make_helper(lgdt) {
 	//uint32_t limit = decode_i_w(eip + 1);
 	//uint32_t base = decode_i_l(eip + 3);
+	printf("$$$$$\n");
 	uint32_t limit = lnaddr_read(eip + 1, 2);
 	uint32_t base = lnaddr_read(eip + 3, 4);
-	printf("$$$$$\n");
 	cpu.gdtr = (((uint64_t)base) << 16) | limit;
 	print_asm("lgdt");
 	return 7;
