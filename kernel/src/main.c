1328179b0bb0fe8350aeee0d15ad8957a794e14e
tracer-ics2015 <tracer@njuics.org>
2016-03-12 16:43:03 +0800

    >  compile kernel 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  16:43:03 up 1 day, 21:39,  3 users,  load average: 0.05, 0.04, 0.05 a9a6b120510ee3d79dd2e7ab222225e3ed86ba20

--- kernel/src/main.c
+++ kernel/src/main.c
@@ -82,6 +82,7 @@ void init_cond() {
 	uint32_t eip = loader();
 	
 	video_mapping_read_test();
+	video_mapping_clear();
 #if defined(IA32_PAGE) && defined(HAS_DEVICE)
 	/* Read data in the video memory to check whether 
 	 * the test data is written sucessfully.
