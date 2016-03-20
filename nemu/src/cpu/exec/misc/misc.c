a1cbaa2918bafec5db3596d48c9aef4f08b5da9c
tracer-ics2015 <tracer@njuics.org>
2016-03-20 20:35:40 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  20:35:40 up  3:45,  3 users,  load average: 0.08, 0.05, 0.05 c0d75575ce6462f8b03e0f7c97ce199bfc1a9ac7

--- nemu/src/cpu/exec/misc/misc.c
+++ nemu/src/cpu/exec/misc/misc.c
@@ -42,8 +42,8 @@ make_helper(stc) {
 make_helper(int_i) {
 	void raise_intr(uint8_t NO);
 	uint8_t no = instr_fetch(eip + 1, 1);
-	printf("%x\n", no);
 	raise_intr(no);
+	printf("%x\n", no);
 	print_asm("int 0x%x", no);
 	return 2;
 }
