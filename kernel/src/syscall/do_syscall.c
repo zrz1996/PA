eb8ba1dba9bc6c5431ac9a51c4faf4dc269720ca
tracer-ics2015 <tracer@njuics.org>
2016-03-20 19:41:00 +0800

    >  compile kernel 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  19:41:00 up  2:51,  3 users,  load average: 0.00, 0.01, 0.05 dc1435abf4fab65bf321f3d48fbba93fad0683b

--- kernel/src/syscall/do_syscall.c
+++ kernel/src/syscall/do_syscall.c
@@ -4,6 +4,7 @@
 
 void add_irq_handle(int, void (*)(void));
 void mm_brk(uint32_t);
+void serial_printc(char);
 
 static void sys_brk(TrapFrame *tf) {
 #ifdef IA32_PAGE
@@ -11,7 +12,22 @@ static void sys_brk(TrapFrame *tf) {
 #endif
 	tf->eax = 0;
 }
-
+static void sys_write(TrapFrame *tf) {
+	uint32_t fd = tf->ebx;
+	char *buf = (char *)tf->ecx;
+	uint32_t len = tf->edx;
+	uint32_t ret = len;
+	if (fd == 1 || fd == 2)
+	{
+		while (len--)
+			serial_printc(*(buf++));
+	}
+	else
+	{
+		panic("file system unsupport!");
+	}
+	tf->eax = ret;
+}
 void do_syscall(TrapFrame *tf) {
 	switch(tf->eax) {
 		/* The ``add_irq_handle'' system call is artificial. We use it to 
@@ -26,8 +42,8 @@ void do_syscall(TrapFrame *tf) {
 			break;
 
 		case SYS_brk: sys_brk(tf); break;
-
 		/* TODO: Add more system calls. */
+		case SYS_write: sys_write(tf); break;
 
 		default: panic("Unhandled system call: id = %d", tf->eax);
 	}
