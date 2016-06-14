6533d7ab6282fb85f6c190172c719d2f45495a6e
tracer-ics2015 <tracer@njuics.org>
2016-06-14 10:41:00 +0800

    >  compile NEMU 14307130222 zhangruizhe Linux ubuntu 3.13.0-32-generic #57~precise1-Ubuntu SMP Tue Jul 15 03:51:20 UTC 2014 x86_64 x86_64 x86_64 GNU/Linux  10:41:00 up 37 min,  1 user,  load average: 0.10, 0.09, 0.17 725b377a9a1ac94b2751f6067dd512205bd6b20a

--- nemu/src/memory/tlb.c
+++ nemu/src/memory/tlb.c
@@ -23,14 +23,16 @@ inline void TLB_insert()
 		uint32_t addr = tag << 12;
 		uint32_t dir_addr = (cpu.cr3.val & 0xfffff000) | ((addr >> 20) & 0xffc);
 		uint32_t dir_entry = hwaddr_read(dir_addr, 4);
-		assert(dir_entry & 1);
+		if ((dir_entry & 1) == 0)
+			continue;
 		uint32_t pg_addr = (dir_entry & 0xfffff000) | ((addr >> 10) & 0xffc);
 		uint32_t pg_entry = hwaddr_read(pg_addr, 4);
-		assert(pg_entry & 1);
+		if ((pg_entry & 1) == 0)
+			continue;
 		TLB[tag] = (pg_entry & 0xfffff000) | 1;
 	}
 	for (tag = 0; tag < NR_ITEM - 1; tag++)
-		if (TLB[tag + 1] == TLB[tag] + 0x1000)
+		if ((TLB[tag] & 1) && (TLB[tag + 1] & 1) && TLB[tag + 1] == TLB[tag] + 0x1000)
 			TLB[tag] |= 2;
 }
 inline hwaddr_t TLB_translate(lnaddr_t addr)
